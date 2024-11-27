2bde18e6070c8eaae227454e39cc3ada15382c42
Aamir Ansari <amirbxr2003@gmail.com>
2024-11-27 16:27:41 +0530

    change for dep

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def predict(input_data: PredictionInput):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app,port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
